ca51f31fb1dabdc42a07ee985611d400724ab860
AlexanderB <123639275+AlexanderBaffert@users.noreply.github.com>
2023-02-15 08:48:03 +0100

    Update Baffert_3M_es-type-hinting.py

--- Baffert_3M_es-type-hinting.py
+++ Baffert_3M_es-type-hinting.py
@@ -6,6 +6,8 @@ import random
 
 #es1
 
+#Creare una funzione descrizione() con due parametri nome ed eta. La funzione restituisce una stringa formattata nel seguente modo: "nome ha eta anni." descrizione("Pippo",23)
+
 def descrizione(nome, eta):
 
     print(f"{nome} ha {eta} anni.")
@@ -13,6 +15,7 @@ def descrizione(nome, eta):
 descrizione(nome = "Marco", eta= "16")
 
 #es2
+#Creare una funzione(?) numero_casuale() che restituisce un numero casuale tra 0 e 99. La funzione restituisce il numero generato numero_casuale()
 
 def numeri_casuali(numeri):
 
@@ -21,6 +24,7 @@ def numeri_casuali(numeri):
 numeri_casuali(numeri=random.randint(0,99))
 
 #es3
+#Creare una funzione descrizione_eta_casuale() con un parametro nome. L'eta e' calcolata in modo casuale La funzione restituisce una stringa formattata nel seguente modo: "nome ha eta anni." descrizione_eta_casuale("Pippo")
 
 def descrizione_eta_casuale(nome):
     
@@ -31,6 +35,7 @@ def descrizione_eta_casuale(nome):
 descrizione_eta_casuale("Gianni")
 
 #es4
+#Creare una funzione(?) descrizione_casuale(). Il nome e' scelto in modo casuale da una lista di nomi interna alla fuzione. L'eta e' calcolata in modo casuale La funzione restituisce una stringa formattata nel seguente modo: "nome ha eta anni." descrizione_casuale()
 
 def descrizione_casuale2(nome):
     global lista #ho dovuto usarlo perche' dava dei problemi per la lista    
